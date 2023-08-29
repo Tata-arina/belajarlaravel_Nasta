ac7ead39934eb85ceb946a93d3f0f60808235eba
Rudi Hartono <rudihar672@gmail.com>
2023-08-29 11:50:45 +0700

    memperbaiki edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,34 +1,38 @@
 @extends('mainapps')
-@section('title') Edit @endsection
+@section('title')
+    Edit
+@endsection
 @section('content')
     <div class="container py-3">
-        <h3 class="fw-bold">Tambahkan Blog</h3>
+        <h3 class="fw-bold">Edit Blog</h3>
         <div class="card">
             <div class="card-body">
                 <form method="post" action="/updateblog/{{ $posts->id }}" enctype="multipart/form-data">
                     @method('PUT')
                     @csrf
-                    <div class= "mb-3">
+                    <div class="mb-3">
                         <label for="penulis" class="form-label">Penulis</label>
-                        <input type="text" class="form-control" id="penulis" name="penulis_post"
-                        value="{{ Auth::user()->name }}" disabled>
+                        <input type="hidden" class="form-control" id="penulis" name="penulis_post"
+                            value="{{ $posts->users_id }}">
+                        <input type="text" class="form-control" value="{{ Auth::user()->name }}" disabled>
                     </div>
-                    <div class= "mb-3">
+                    <div class="mb-3">
                         <label for="judul_post" class="form-label">Judul Post</label>
                         <input type="text" class="form-control" id="judul_post" name="judul_post"
-                        value="{{ $posts->judul_post}}">
+                            value="{{ $posts->judul_post }}">
                     </div>
-                    <div class= "mb-3">
+                    <div class="mb-3">
                         <label for="isi_post" class="form-label">Isi Post</label>
                         <textarea class="form-control" id="isi_post" cols="30" rows="5" name="isi_post">
-                        {{ $posts->isi_post}}
+                        {{ $posts->isi_post }}
                         </textarea>
                     </div>
-                    <div class= "mb-3">
+                    <div class="mb-3">
                         <label for="gambar_post" class="form-label">Gambar Post</label>
                         <input type="file" class="form-control" id="gambar_post" name="gambar_post" accept="image/*">
-                        <input type="hidden" name="oldImage"  value="{{ $posts->gambar_post }}">
-                        <img src="{{ asset('storage/' . $posts->gambar_post) }}" class="img-thumbhnail mt-3" width="40%" alt="foto">
+                        <input type="hidden" name="oldImage" value="{{ $posts->gambar_post }}">
+                        <img src="{{ asset('storage/' . $posts->gambar_post) }}" class="img-thumbhnail mt-3" width="40%"
+                            alt="foto">
                     </div>
                     <div class="text-end">
                         <a href="/home" class="btn btn-secondary">Kembali</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,46 +1,53 @@
 @extends('mainapps')
-@section('title')Home @endsection
+@section('title')
+    Home
+@endsection
 @section('content')
-  <section id="header" class="header">
-    <div class="container">
-      <h1 class="judul-header"><p class="text-secondary-emphasis fw-bold">Selamat Datang Di <br> Blog Nasta</h1>
-    </div>
-  </section>
-
-  <section class="py-5" id="blog">
-    <div class="container">
-      <a href="/createstts" class="btn btn-secondary fw-bold">Tambahkan Blog Anda</a>
-      @foreach ($posts as $post)
-      <div class="card mb-3 mt-3" style="max-width: 100%;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="{{ asset('storage/' . $post->gambar_post) }}" class="img-fluid rounded-start" alt="...">
-    </div>
-
-    <div class="col-md-8">
-      <div class="card-body">
-        <div class="d-flex justify-content-end" role="group">
-          <button type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-three-dots"></i>
-          </button>
-            <ul class="dropdown-menu">
-              @if (Auth::user()->id === $post->users_id)
-              <li><a class="dropdown-item" href="/edit/{{ $post->id}}">Edit</a></li>
-              <li><a class="dropdown-item" href="/delete/{{ $post->id}}">Delete</a></li>
-              @else
-              <li><a class="dropdown-item" href="">Bagikan</a></li>
-              @endif
-            </ul>
+    <section id="header" class="header">
+        <div class="container">
+            <h1 class="judul-header">
+                <p class="text-secondary-emphasis fw-bold">Selamat Datang Di <br> Blog Nasta
+            </h1>
         </div>
-              <h5 class="card-title fw-bold">{{ $post->judul_post }}</h5>
-            <p class="card-text">{{ $post->isi_post }}</p>
-            <p class="card-text"><small class="text-body-secondary">{{ $post->users['name'] }} </small> <a href="" class="btn"><i class="bi bi-heart"></i> like</a> <a href="" class="btn"><i class="bi bi-chat-text"></i>Comment</a></p>
-      </div>
-    </div>
-  </div>
-</div>
-  @endforeach
-    </div>
     </section>
 
+    <section class="py-5" id="blog">
+        <div class="container">
+            <a href="/createstts" class="btn btn-secondary fw-bold">Tambahkan Blog Anda</a>
+            @foreach ($posts as $post)
+                <div class="card mb-3 mt-3" style="max-width: 100%;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{ asset('storage/' . $post->gambar_post) }}" class="img-fluid rounded-start"
+                                alt="...">
+                        </div>
+
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-end" role="group">
+                                    <button type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-three-dots"></i>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        @if (Auth::user()->id === $post->users_id)
+                                            <li><a class="dropdown-item" href="/edit/{{ $post->id }}">Edit</a></li>
+                                            <li><a class="dropdown-item" href="/delete/{{ $post->id }}">Delete</a></li>
+                                        @else
+                                            <li><a class="dropdown-item" href="">Bagikan</a></li>
+                                        @endif
+                                    </ul>
+                                </div>
+                                <h5 class="card-title fw-bold">{{ $post->judul_post }}</h5>
+                                <p class="card-text">{{ $post->isi_post }}</p>
+                                <p class="card-text"><small class="text-body-secondary">{{ $post->users['name'] }} </small>
+                                    <a href="" class="btn"><i class="bi bi-heart"></i> like</a> <a href=""
+                                        class="btn"><i class="bi bi-chat-text"></i>Comment</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
